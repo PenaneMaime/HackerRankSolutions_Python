2d662592986191cc5e20d36b734a4318021ebf00
PenaneMaime <penanemaime@gmail.com>
2022-04-07 12:41:02 +0200

    Prints numbers upto N as one string

---
+++ printNumsToN.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    i=1
+    numbersString=""
+    
+    while i<=n :
+        numbersString=numbersString+str(i)
+        i+=1
+    
+    print(numbersString)
